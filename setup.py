88f5635e2e28adc44ce02f5fbd694cbe1b197a7e
arjan <arjanvanbalken@gmail.com>
2019-11-08 19:52:00 +0100

    Remove email address from setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="arris-tg2492lg-vanbalken",
     version="0.1.0",
-    author="Arjan van Balken",
-    author_email="arjanvanbalken@protonmail.com",
+    author="vanbalken",
     description="Python client for the Arris TG2492LG",
     long_description=long_description,
     long_description_content_type="text/markdown",
