1258d0d19f96fad1f86e0125d40ee4a6566bc19b
arjan <arjanvanbalken@gmail.com>
2020-01-18 16:08:23 +0100

    Show help when called without arguments

--- examples/poll_device_status.py
+++ examples/poll_device_status.py
@@ -1,5 +1,6 @@
 import argparse
 import sched
+import sys
 
 try:
     from arris_tg2492lg import ConnectBox  # The typical way to import arris_tg2492lg
@@ -19,6 +20,11 @@ def main():
     parser.add_argument("--host", action="store", dest="host", help="ip-address of the router")
     parser.add_argument("--password", action="store", dest="password", help="password of the router")
     parser.add_argument("--mac", action="store", dest="mac_address", help="mac address of device to poll")
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        exit(1)
+
     args = parser.parse_args()
 
     connect_box = ConnectBox(args.host, args.password)
