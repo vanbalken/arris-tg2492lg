6ea20e96c4f784194b14508fd8b4eff5573ead70
Arjan van Balken <arjanvanbalken@gmail.com>
2022-05-25 21:04:26 +0200

    Update and pin libraries

--- tests/test_connect_box.py
+++ tests/test_connect_box.py
@@ -9,8 +9,8 @@ from arris_tg2492lg.connect_box import ConnectBox
 from arris_tg2492lg.exception import InvalidCredentialError
 
 
-async def test_async_login_ok(aiohttp_client, loop):
-    app = web.Application(loop=loop)
+async def test_async_login_ok(aiohttp_client):
+    app = web.Application()
     app.router.add_get("/login", _get_credential)
     client = await aiohttp_client(app)
 
@@ -20,16 +20,16 @@ async def test_async_login_ok(aiohttp_client, loop):
     assert token == "eyJuYW1lIjogImFkbWluIn0="  # base64 representation of: {"name": "admin"}
 
 
-async def test_async_login_nok_wrong_password(aiohttp_client, loop):
+async def test_async_login_nok_wrong_password(aiohttp_client):
     """Test when an invalid password is provided.
 
     The router responds with an Internal Server Error when a wrong password is provided.
     """
 
-    def internal_server_error(request):
+    async def internal_server_error(request):
         return web.Response(status=500)
 
-    app = web.Application(loop=loop)
+    app = web.Application()
     app.router.add_get("/login", internal_server_error)
     client = await aiohttp_client(app)
 
@@ -39,17 +39,17 @@ async def test_async_login_nok_wrong_password(aiohttp_client, loop):
         await connect_box.async_login()
 
 
-async def test_async_login_nok_html_response(aiohttp_client, loop):
+async def test_async_login_nok_html_response(aiohttp_client):
     """Test when an invalid endpoint is used.
 
     An unsupported browser could respond with html.
     """
 
-    def get_html_response(request):
+    async def get_html_response(request):
         html_response = "<!DOCTYPE html><html><body>hello</body></html>"
         return web.Response(text=html_response)
 
-    app = web.Application(loop=loop)
+    app = web.Application()
     app.router.add_get("/login", get_html_response)
     client = await aiohttp_client(app)
 
@@ -59,8 +59,8 @@ async def test_async_login_nok_html_response(aiohttp_client, loop):
         await connect_box.async_login()
 
 
-async def test_get_connected_devices(aiohttp_client, loop):
-    app = web.Application(loop=loop)
+async def test_get_connected_devices(aiohttp_client):
+    app = web.Application()
     app.router.add_get("/login", _get_credential)
     app.router.add_get("/getConnDevices", _get_mock_data)
     client = await aiohttp_client(app)
@@ -71,22 +71,22 @@ async def test_get_connected_devices(aiohttp_client, loop):
     assert len(connected_devices) == 4
 
 
-async def test_get_connected_devices_throws_401_once(aiohttp_client, loop):
-    def login_result(request):
+async def test_get_connected_devices_throws_401_once(aiohttp_client):
+    async def login_result(request):
         login_result.call_count += 1
-        return _get_credential(request)
+        return await _get_credential(request)
 
-    def conn_devices_result(request):
+    async def conn_devices_result(request):
         conn_devices_result.call_count += 1
         if conn_devices_result.call_count == 1:
             return web.Response(status=401)
         else:
-            return _get_mock_data(request)
+            return await _get_mock_data(request)
 
     login_result.call_count = 0
     conn_devices_result.call_count = 0
 
-    app = web.Application(loop=loop)
+    app = web.Application()
     app.router.add_get("/login", login_result)
     app.router.add_get("/getConnDevices", conn_devices_result)
     client = await aiohttp_client(app)
@@ -100,19 +100,19 @@ async def test_get_connected_devices_throws_401_once(aiohttp_client, loop):
     assert conn_devices_result.call_count == 2
 
 
-async def test_get_connected_devices_throws_401_twice(aiohttp_client, loop):
-    def login_result(request):
+async def test_get_connected_devices_throws_401_twice(aiohttp_client):
+    async def login_result(request):
         login_result.call_count += 1
-        return _get_credential(request)
+        return await _get_credential(request)
 
-    def conn_devices_result(request):
+    async def conn_devices_result(request):
         conn_devices_result.call_count += 1
         return web.Response(status=401)
 
     login_result.call_count = 0
     conn_devices_result.call_count = 0
 
-    app = web.Application(loop=loop)
+    app = web.Application()
     app.router.add_get("/login", login_result)
     app.router.add_get("/getConnDevices", conn_devices_result)
     client = await aiohttp_client(app)
@@ -126,14 +126,14 @@ async def test_get_connected_devices_throws_401_twice(aiohttp_client, loop):
     assert conn_devices_result.call_count == 2
 
 
-async def test_logout_accepts_http_status_500(aiohttp_client, loop):
-    def get_logout_success(request):
+async def test_logout_accepts_http_status_500(aiohttp_client):
+    async def get_logout_success(request):
         get_logout_success.call_count += 1
         return web.Response(status=500)
 
     get_logout_success.call_count = 0
 
-    app = web.Application(loop=loop)
+    app = web.Application()
     app.router.add_get("/login", _get_credential)
     app.router.add_get("/logout", get_logout_success)
     client = await aiohttp_client(app)
@@ -144,14 +144,14 @@ async def test_logout_accepts_http_status_500(aiohttp_client, loop):
     assert get_logout_success.call_count == 1
 
 
-async def test_logout_throws_for_401_unauthorized(aiohttp_client, loop):
+async def test_logout_throws_for_401_unauthorized(aiohttp_client):
     async def get_logout_failure(request):
         get_logout_failure.call_count += 1
         return web.Response(status=401)
 
     get_logout_failure.call_count = 0
 
-    app = web.Application(loop=loop)
+    app = web.Application()
     app.router.add_get("/login", _get_credential)
     app.router.add_get("/logout", get_logout_failure)
     client = await aiohttp_client(app)
@@ -164,8 +164,8 @@ async def test_logout_throws_for_401_unauthorized(aiohttp_client, loop):
     assert get_logout_failure.call_count == 1
 
 
-async def test_get_router_information(aiohttp_client, loop):
-    app = web.Application(loop=loop)
+async def test_get_router_information(aiohttp_client):
+    app = web.Application()
     app.router.add_get("/login", _get_credential)
     app.router.add_get("/snmpGet", _get_mock_router_information)
     client = await aiohttp_client(app)
@@ -179,20 +179,20 @@ async def test_get_router_information(aiohttp_client, loop):
     assert router_information.serial_number == "ABCD12345678"
 
 
-def _get_credential(request):
+async def _get_credential(request):
     dummy_token = base64.b64encode('{"name": "admin"}'.encode("utf-8")).decode("ascii")
 
     return web.Response(text=dummy_token)
 
 
-def _get_mock_data(request):
+async def _get_mock_data(request):
     current_path = Path(os.path.dirname(os.path.realpath(__file__)))
     test_data_path = current_path / "getConnDevices-response.json"
 
     return web.Response(text=test_data_path.read_text())
 
 
-def _get_mock_router_information(request):
+async def _get_mock_router_information(request):
     current_path = Path(os.path.dirname(os.path.realpath(__file__)))
     test_data_path = current_path / "snmpGet-response.json"
 
